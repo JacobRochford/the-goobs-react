ca635c9c5d2b56ebd5fec98bf88b547c7d8b53f4
JacobRochford <jrochford357@gmail.com>
2022-01-05 09:21:48 -0800

    add footer component

---
+++ src/components/layout/Footer.js
@@ -0,0 +1,41 @@
+// import { Link } from 'react-router-dom';
+
+import './Footer.module.css';
+
+function Footer() {
+  return (
+    <footer>
+      <div>
+        <h3>The Goobs</h3>
+        <ul>
+          <li>
+            <a href="https://www.youtube.com" target="_blank" rel="noreferrer">
+              <img
+                src={require("../../images/YoutubeLogo.png")}
+                alt="youtube"
+              />
+            </a>
+          </li>
+          <li>
+            <a href="https://www.spotify.com" target="_blank" rel="noreferrer">
+              <img
+                src={require("../../images/Spotify Logo.png")}
+                alt="spotify"
+              />
+            </a>
+          </li>
+          <li>
+            <a href="https://www.facebook.com" target="_blank" rel="noreferrer">
+              <img
+                src={require("../../images/Facebook Logo.png")}
+                alt="facebook"
+              />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
