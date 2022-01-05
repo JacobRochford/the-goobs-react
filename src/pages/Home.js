085537d71a2cd3095a9da1729129f4335dc59d2f
JacobRochford <jrochford357@gmail.com>
2022-01-05 09:20:39 -0800

    add homepage

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import MainNavigation from "../components/layout/MainNavigation";
+import Tours from "../components/layout/Tours";
+import Footer from "../components/layout/Footer";
+
+function Homepage() {
+  return <div>
+    <MainNavigation />
+    <Tours />
+    <Footer />
+  </div>
+}
+
+export default Homepage;
