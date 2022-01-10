3966c631030c02d9274d77dfd104ab7888363189
JacobRochford <jrochford357@gmail.com>
2022-01-09 18:13:51 -0800

    add tour component

---
+++ src/components/tours/TourItem.js
@@ -0,0 +1,20 @@
+import styles from './TourItem.module.css';
+
+function TourItem() {
+  // props.forEach(tour => {
+  //   return (
+  //     <div className={styles.tourRow}>
+  //       <span className={styles.tourDate}>{tour.date}</span>
+  //       <span className={styles.tourCity}>{tour.city}</span>
+  //       <span className={styles.tourArena}>{tour.arena}</span>
+  //       <button className={styles.buyTicketButton}>
+  //         BUY TICKETS
+  //       </button>
+  //       <hr className={styles.line} />
+  //     </div>
+  //   )
+  // })
+
+}
+
+export default TourItem;
