6a255751490448946fe9db8183f0fe32b6c23fee
JacobRochford <jrochford357@gmail.com>
2022-01-10 17:53:12 -0800

    update app component

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import { Route, Routes } from 'react-router-dom';
 import AboutPage from './pages/About';
 import Homepage from './pages/Home';
 import StorePage from './pages/Store';
+import Layout from './components/layout/Layout';
 
 function App() {
   return (
+    <Layout>
       <Routes>
         <Route path={"/"} element={<Homepage />} />
         <Route path={"/store"} element={<StorePage />} />
         <Route path={"/about"} element={<AboutPage />} />
       </Routes>
+    </Layout>
   );
 }
 
