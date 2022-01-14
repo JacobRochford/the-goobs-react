983ec3b9cc8cf413cdd03b9b63c34c83cab06649
JacobRochford <jrochford357@gmail.com>
2022-01-13 17:55:37 -0800

    update store page

--- src/pages/Store.js
+++ src/pages/Store.js
@@ -1,11 +1,7 @@
-import MainNavigation from "../components/layout/MainNavigation";
-import Footer from "../components/layout/Footer";
 
 function StorePage() {
   return (
     <div>
-      <MainNavigation />
-      <Footer />
     </div>
   );
 }
