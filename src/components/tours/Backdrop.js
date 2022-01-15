ae7d3f6acd0ce2eb86a50885d61894c4182a2b77
JacobRochford <jrochford357@gmail.com>
2022-01-14 17:24:38 -0800

    update component

--- src/components/tours/Backdrop.js
+++ src/components/tours/Backdrop.js
@@ -1,7 +1,7 @@
 import styles from './Backdrop.module.css';
 
 function Backdrop() {
-  return <div className={styles.backdrop}></div>
+  return <div className={styles.backdrop}> </div>
 }
 
 export default Backdrop;
