6038e52faa13a905c74fab5746bc0f4f769190e3
JacobRochford <jrochford357@gmail.com>
2022-01-20 16:05:22 -0800

    update homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,49 @@
 import Tours from "../components/tours/Tours";
 
+const TOUR_DATA = [
+  {
+    id: "123",
+    date: "Jan 16",
+    city: "San Diego, CA",
+    arena: "Pechanga Arena",
+  },
+  {
+    id: "234",
+    date: "Jan 19",
+    city: "Los Angeles, CA",
+    arena: "Crypto.com",
+  },
+  {
+    id: "345",
+    date: "Jan 22",
+    city: "Portland, OR",
+    arena: "Moda Center",
+  },
+  {
+    id: "456",
+    date: "Feb 16",
+    city: "Las Vegas, NV",
+    arena: "T-Mobile Arena",
+  },
+  {
+    id: "567",
+    date: "Feb 29",
+    city: "Detroit, MI",
+    arena: "Little Caesars Arena",
+  },
+  {
+    id: "678",
+    date: "Mar 17",
+    city: "Baltimore, MD",
+    arena: "Royal Farms Arena",
+  },
+];
+
 function Homepage() {
   return (
     <div>
       <h2>TOURS</h2>
-      <Tours />
+      <Tours tours={TOUR_DATA} />
     </div>
   );
 }
