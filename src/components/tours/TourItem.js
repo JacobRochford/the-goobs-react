99b5d9ad422297534ce530add74ff7d72fba8cc9
JacobRochford <jrochford357@gmail.com>
2022-01-20 16:04:35 -0800

    update tour item

--- src/components/tours/TourItem.js
+++ src/components/tours/TourItem.js
@@ -1,19 +1,19 @@
 import styles from './TourItem.module.css';
 
-function TourItem() {
-  // props.forEach(tour => {
-  //   return (
-  //     <div className={styles.tourRow}>
-  //       <span className={styles.tourDate}>{tour.date}</span>
-  //       <span className={styles.tourCity}>{tour.city}</span>
-  //       <span className={styles.tourArena}>{tour.arena}</span>
-  //       <button className={styles.buyTicketButton}>
-  //         BUY TICKETS
-  //       </button>
-  //       <hr className={styles.line} />
-  //     </div>
-  //   )
-  // })
+function TourItem({id, date, city, arena}) {
+  return (
+      <div>
+        <div className={styles.tourRow} id={id}>
+          <span className={styles.tourDate}>{date}</span>
+          <span className={styles.tourCity}>{city}</span>
+          <span className={styles.tourArena}>{arena}</span>
+          <button className={styles.buyTicketButton}>
+            BUY TICKETS
+          </button>
+        </div>
+        <hr className={styles.line} />
+      </div>
+    )
 
 }
 
