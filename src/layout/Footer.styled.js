f99e4652f440fc1f014ca859334ab751dc3210fb
JacobRochford <jrochford357@gmail.com>
2022-02-07 14:33:51 -0800

    minor styling fixes

--- src/layout/Footer.styled.js
+++ src/layout/Footer.styled.js
@@ -1,33 +1,35 @@
 import styled from "styled-components";
 
 export const StyledDiv = styled.div`
+  display: flex;
+  align-items: center;
+  height: 220px;
+//   max-width: 1440px;
+  margin: auto;
+  h3 {
+    font-family: 'Booter - Zero Zero';
+    font-size: 4rem;
+    text-align: center;
+    margin: 0 5rem 0 5rem;
+    color: #fff;
+    font-weight: normal;
+  }
+
+  ul {
     display: flex;
-    align-items: center;
+    flex-grow: 1;
+    justify-content: flex-end;
+    padding-right: 5%;
+    list-style-type: none;
+  }
 
-    h3 {
-        font-family: 'Booter - Zero Zero';
-        font-size: 4rem;
-        text-align: center;
-        margin: 5%;
-        color: #fff;
-        font-weight: normal;
-    }
+  li {
+    padding: 0 1em;
+  }
 
-    ul {
-        display: flex;
-        flex-grow: 1;
-        justify-content: flex-end;
-        padding-right: 5%;
-        list-style-type: none;
-    }
-    
-    li{
-        padding: 0 1em;
-    }
-    
-    .img {
-        color: #fff;
-        width: 30px;
-        height: 30px;
-    }
-`
+  .img {
+    color: #fff;
+    width: 30px;
+    height: 30px;
+  }
+`;
