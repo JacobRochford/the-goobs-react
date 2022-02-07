a52e2a8447ecf2b721703ea70b37b7f3f671b801
JacobRochford <jrochford357@gmail.com>
2022-02-07 14:34:27 -0800

    add site theme

---
+++ src/theme/theme.js
@@ -0,0 +1,20 @@
+const theme = {
+  colors: {
+    
+  },
+
+  fonts: {
+    titles: "Booter - Zero Zero",
+    headers: "Metal Mania",
+    body: "Raleway"
+  },
+
+  breakpoints: {
+    sm: "screen and (max-width: 640px)",
+    md: "screen and (min-width: 641px) and (max-width: 768px)",
+    lg: "screen and (min-width: 769px) and (max-width: 1024px)",
+    xl: "screen and (min-width: 1025px) and (max-width: 1280px)",
+  },
+};
+
+export default theme;
