7dd2d561edacb79f7cac7f84773b1799c55d15f3
JacobRochford <jrochford357@gmail.com>
2022-02-09 07:39:48 -0800

    style music section

---
+++ src/components/store/Music.styled.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+
+export const StyledMusic = styled.div`
+  h2 {
+    font-family: 'Metal Mania';
+    text-align: center;
+    font-size: 2rem;
+    color: #111;
+    padding-top: 2rem;
+  }
+
+  section {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+  }
+`;
