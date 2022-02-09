0d08ac55072ba622bc5fc2c5c7b80ebb867bc9dc
JacobRochford <jrochford357@gmail.com>
2022-02-09 07:39:24 -0800

    style merch section

---
+++ src/components/store/Merch.styled.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const StyledMerch = styled.div`
+  h2 {
+    font-family: 'Metal Mania';
+    text-align: center;
+    font-size: 2rem;
+    color: #111;
+    padding-top: 2rem;
+  }
+
+  section {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+  }
+`;
