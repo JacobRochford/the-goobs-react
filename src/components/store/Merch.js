04ff58d480338a7d6542182ccc391e345f6e9f11
JacobRochford <jrochford357@gmail.com>
2022-02-09 07:39:09 -0800

    add merch section

---
+++ src/components/store/Merch.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { StyledMerch } from './Merch.styled';
+
+const Merch = () => {
+  return (
+    <StyledMerch>
+      <h2>MERCH</h2>
+      <section>
+        
+      </section>
+    </StyledMerch>
+  );
+};
+
+export default Merch;
