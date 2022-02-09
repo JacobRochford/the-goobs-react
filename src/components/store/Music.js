c48446b4051051cfdeb11cbc49f9ba864cb1002c
JacobRochford <jrochford357@gmail.com>
2022-02-09 07:39:38 -0800

    add music section

---
+++ src/components/store/Music.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { StyledMusic } from './Music.styled';
+
+const Music = () => {
+  return (
+    <StyledMusic>
+      <h2>MUSIC</h2>
+      <section>
+        
+      </section>
+    </StyledMusic>
+  );
+};
+
+export default Music;
