bea6e3731a4c03298f93cfda930be6933fb282d3
JacobRochford <jrochford357@gmail.com>
2022-02-16 21:04:49 -0800

    add merch items to store

--- src/components/store/Merch.js
+++ src/components/store/Merch.js
@@ -1,15 +1,34 @@
 import React from 'react';
 import { StyledMerch } from './Merch.styled';
+import { StyledShopItem } from './ShopItem.styled';
+// import { server } from '../../config';
 
-const Merch = () => {
+
+const Merch = ({ merch }) => {
+  console.log(merch)
   return (
     <StyledMerch>
       <h2>MERCH</h2>
       <section>
-        
+        {merch.map((item) => {
+          return (
+            <StyledShopItem key={item.id}>
+              <h3>{item.name}</h3>
+              <img src={item.image} width={300} height={300} />
+              <div>
+                <span>{item.price}</span>
+                <button>ADD TO CART</button>
+              </div>
+            </StyledShopItem>
+          );
+        })}
       </section>
     </StyledMerch>
   );
 };
+
+// export const getStaticProps = async () => {
+//   const res = await fetch()
+// }
 
 export default Merch;
