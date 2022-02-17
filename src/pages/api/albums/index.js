6339517970154556314809647c4930e1d476a49e
JacobRochford <jrochford357@gmail.com>
2022-02-16 19:26:56 -0800

    configure api endpoint to return album data

--- src/pages/api/albums/index.js
+++ src/pages/api/albums/index.js
@@ -1,14 +1,15 @@
 import { ALBUM_DATA } from "../../../../albumdata";
+import { server } from "../../../../config";
 
 export default function handler(req, res) {
   res.status(200).json(ALBUM_DATA)
 }
 
-export const getStaticProps = async () => {
-  const res = await fetch(`${server}/api/albums`);
-  const albums = await res.json();
+// export const getStaticProps = async () => {
+//   const res = await fetch(`${server}/api/albums`);
+//   const albums = await res.json();
 
-  return {
-    albums
-  };
-};
+//   return {
+//     albums
+//   };
+// };
