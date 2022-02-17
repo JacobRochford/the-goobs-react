4fb61e7513a0bb069dddb3e779134f0fc7f08f29
JacobRochford <jrochford357@gmail.com>
2022-02-16 20:58:09 -0800

    add site data

---
+++ albumdata.js
@@ -0,0 +1,38 @@
+export const ALBUM_DATA = [
+  {
+    id: 1,
+    name: 'Goobies',
+    image: '/images/Goob-Album4.JPG',
+    price: '14.99',
+  },
+  {
+    id: 2,
+    name: 'Gibbs',
+    image: '/images/Goob-N.jpg',
+    price: '13.99',
+  },
+  {
+    id: 3,
+    name: 'Goopie',
+    image: '/images/Goob-J.JPG',
+    price: '11.99',
+  },
+  {
+    id: 4,
+    name: 'Goobs Vol. 1',
+    image: '/images/Goobs-Album.jpg',
+    price: '16.99',
+  },
+  {
+    id: 5,
+    name: 'Goobsters',
+    image: '/images/Goobs-Album3.png',
+    price: '14.99',
+  },
+  {
+    id: 6,
+    name: 'Goobs Vol. 2',
+    image: '/images/Goobs-Album2.jpg',
+    price: '17.99',
+  },
+];
---
+++ merchdata.js
@@ -0,0 +1,26 @@
+export const MERCH_DATA = [
+    {
+        "id": 7,
+        "name": "Goob Sticker",
+        "image": "/images/goobs-item3.jpg",
+        "price": "4.99"
+    },
+    {
+        "id": 8,
+        "name": "Goobs Mug 10oz",
+        "image": "/images/goobs-coffee.jpg",
+        "price": "9.99"
+    },
+    {
+        "id": 9,
+        "name": "Goobs Mug 12oz",
+        "image": "/images/goobs-coffee3.jpg",
+        "price": "10.99"
+    },
+    {
+        "id": 10,
+        "name": "Goobs Mints",
+        "image": "/images/goobs-item.jpg",
+        "price": "6.99"
+    }
+]
