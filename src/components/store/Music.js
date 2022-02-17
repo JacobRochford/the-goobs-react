a913ad5aff709d634dba87cf2e182d1a9b4daa1b
JacobRochford <jrochford357@gmail.com>
2022-02-16 21:04:13 -0800

    add music items to store

--- src/components/store/Music.js
+++ src/components/store/Music.js
@@ -1,15 +1,43 @@
 import React from 'react';
 import { StyledMusic } from './Music.styled';
+import ShopItem from './ShopItem';
+// import albums from '../../../albumdata'
+import { server } from '../../../config';
+import Image from 'next/image';
+import { StyledShopItem } from './Music.styled';
 
-const Music = () => {
+const Music = ({albums}) => {
+  console.log(albums)
   return (
     <StyledMusic>
       <h2>MUSIC</h2>
       <section>
-        
+        {albums.map((album) => {
+          return (
+            <StyledShopItem key={album.id}>
+              <h3>{album.name}</h3>
+              <img src={album.image} width={300} height={300} />
+              <div>
+                <span>{album.price}</span>
+                <button>ADD TO CART</button>
+              </div>
+            </StyledShopItem>
+          );
+        })}
       </section>
     </StyledMusic>
   );
 };
+
+// export const getStaticProps = async () => {
+//   const res = await fetch(`${server}/api/albums`);
+//   const albums = await res.json();
+
+//   return {
+//     props: {
+//       albums: {albums},
+//     },
+//   };
+// };
 
 export default Music;
--- src/components/store/Music.styled.js
+++ src/components/store/Music.styled.js
@@ -15,3 +15,40 @@ export const StyledMusic = styled.div`
     grid-template-columns: repeat(2, 1fr);
   }
 `;
+
+export const StyledShopItem = styled.div`
+  text-align: center;
+  margin-bottom: 2.5rem;
+
+  h3 {
+    color: #555;
+    font-size: 1.2rem;
+  }
+
+  img {
+    height: 250px;
+    width: 250px;
+    box-shadow: 2px 2px 5px;
+  }
+
+  span {
+    font-size: 1.2rem;
+    color: #000;
+  }
+
+  button {
+    background-color: rgb(23, 200, 253);
+    border-radius: 15px;
+    cursor: pointer;
+    font-size: 0.8rem;
+    text-transform: uppercase;
+    color: #333;
+    padding: 0.3rem;
+    margin: 0.5rem;
+    box-shadow: 1px 1px 3px;
+  }
+
+  button:hover {
+    background-color: rgba(24, 247, 255, 0.7);
+  }
+`;
